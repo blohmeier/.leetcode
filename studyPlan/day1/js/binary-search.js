12ab775879db5dbffe6324fa333ec941885a5eda
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-04 08:32:24 -0600

    adds details to first day1 problem

--- studyPlan/day1/js/binary-search.js
+++ studyPlan/day1/js/binary-search.js
@@ -12,8 +12,18 @@ var search = function(nums, target) {
         let m = Math.floor((l + r) / 2); // middle of l and r
         if (nums[m] < target) { // #1
             l = m + 1;
+            console.log("middle is: " + m);
+            console.log("index of middle is less than target, and is: " + nums[m]);
+            console.log("left is: " + l);
+            console.log("right is: " + r);
+            console.log("length is: " + nums.length);
         } else if (nums[m] > target) { // #2
             r = m - 1;
+            console.log("middle is: " + m);
+            console.log("index of middle is greater than target, and is: " + nums[m]);
+            console.log("left is: " + l);
+            console.log("right is: " + r);
+            console.log("length is: " + nums.length);
         } else { // #3
             return m;
         }
@@ -22,3 +32,4 @@ var search = function(nums, target) {
 };
 
 console.log(search([-1,0,3,5,9,12],9));
+console.log(search([-1,0,3,5,9,12],2));
