e36893d969d31512d819944392573c798cdb85e7
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-08 09:58:00 -0600

    completes 876. Middle of the Linked List

--- middleOfTheLinkedList.js
+++ middleOfTheLinkedList.js
@@ -1,11 +1,19 @@
-"use strict";
-
-function middleNode(head) {
-    let A = [head];
-    while (A[A.length - 1].next != null)
-        A.push(A[A.length - 1].next);
-    var listResult = A[Math.trunc(A.length / 2)];
-    return listResult;
+`use strict`;
+//Attempt 1
+// function middleNode(head) {
+//     let A = [head];
+//     while (A[A.length - 1].next != null)
+//         A.push(A[A.length - 1].next);
+//     return A[Math.trunc(A.length / 2)];
+// };
+//Attempt 2
+var middleNode = function(head) {
+    let fast = head;
+    let slow = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
 };
-console.log(middleNode([1,2,3,4,5]));
-console.log(listResult);
+middleNode([1,2,3,4,5]);
