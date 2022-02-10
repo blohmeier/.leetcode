d58179e5e9dc7ee062dd0988facad10959b40627
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-10 06:45:41 -0600

    completes Longest Substring Without Repeating Chars

--- longestSubstring_3.js
+++ longestSubstring_3.js
@@ -24,3 +24,45 @@ Constraints:
 s consists of English letters, digits, symbols and spaces.
  */
 
+// function lengthOfLongestSubstring (s) {
+//     for (let i = 0; i = s.length; i++) {
+//         if (s[i] !== s[i+1]) {
+//             var result = "" + s[i];
+//         }
+//     }
+//     return result;
+// }
+// const lengthOfLongestSubstring = s => {
+//     // track most recent index of each letter
+//     const map = {};
+//     // track starting index of the current substring
+//     var left = 0;
+//
+//     return s.split('').reduce((max, v, i) => {
+//         // starting index of substring is 1 + (the last index of this letter) to ensure this letter is not counted twice.
+//         left = map[v] >= left ? map[v] + 1 : left;
+//         // updates last recorded index of letter to the most recent index.
+//         map[v] = i;
+//
+//         // indices of current substring is (idx - leftIdx, idx).
+//         // +1 because if your substring starts and ends at index 0, it still has a length of 1.
+//         return Math.max(max, i - left + 1);
+//     }, 0)
+// };
+function lengthOfLongestSubstring(s) {
+    let seen = new Set();
+    let longest = 0;
+    let l = 0;
+    for (let r = 0; r < s.length; r++) {
+        while (seen.has(s[r])) {
+            seen.delete(s[l]);
+            l++;
+        }
+        seen.add(s[r]);
+        longest = Math.max(longest, r - l + 1);
+    }
+    return longest;
+};
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
