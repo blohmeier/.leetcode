bef901d5a8f240452dc04e32aef7290d187fce34
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-10 07:14:36 -0600

    completes Max Area of Island

--- maxAreaOfIsland_695.js
+++ maxAreaOfIsland_695.js
@@ -3,9 +3,7 @@
 Medium
 
 You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
-
 The area of an island is the number of cells with a value 1 in the island.
-
 Return the maximum area of an island in grid. If there is no island, return 0.
 
 Example 1:
@@ -13,11 +11,9 @@ Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,
 Output: 6
 Explanation: The answer is not 11, because the island must be connected 4-directionally.
 
-
 Example 2:
 Input: grid = [[0,0,0,0,0,0,0,0]]
 Output: 0
-
 
 Constraints:
 
@@ -26,3 +22,15 @@ n == grid[i].length
 1 <= m, n <= 50
 grid[i][j] is either 0 or 1.
  */
+var maxAreaOfIsland = function(grid) {
+    let ans = 0, n = grid.length, m = grid[0].length
+    const trav = (i, j) => {
+        if (i < 0 || j < 0 || i >= n || j >= m || !grid[i][j]) return 0
+        grid[i][j] = 0
+        return 1 + trav(i-1, j) + trav(i, j-1) + trav(i+1, j) + trav(i, j+1)
+    }
+    for (let i = 0; i < n; i++)
+        for (let j = 0; j < m; j++)
+            if (grid[i][j]) ans = Math.max(ans, trav(i, j))
+    return ans
+};
