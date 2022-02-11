c92e91f6cd06011e29e8cc0bc4102db5d7a57a67
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-11 08:39:15 -0600

    adds  to review11Feb

--- review11Feb.js
+++ review11Feb.js
@@ -22,3 +22,16 @@ function primeGenerator () {
     }
 }
 primeGenerator ();
+
+//output the first 50 numbers in the fibonacci sequence
+
+/*704. Binary Search: Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.*/
+function binarySearch(nums, target) {
+    let lp = 0;
+    let rp = nums.length - 1;
+    while (l <= r) {
+        let middle = Math.floor((lp + rp)/2);
+
+    }
+}
