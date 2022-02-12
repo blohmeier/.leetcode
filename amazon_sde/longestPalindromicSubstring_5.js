02e78cab626766dd7a9517ca873c97cd8f81b419
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-12 09:33:33 -0600

    completes

--- amazon_sde/longestPalindromicSubstring_5.js
+++ amazon_sde/longestPalindromicSubstring_5.js
@@ -16,3 +16,35 @@ Constraints:
 1 <= s.length <= 1000
 s consist of only digits and English letters.
 */
+//Solution 1:
+// function longestPalindrome(s) {
+//     // ll: Left index of the longest palindrome.
+//     // rr: Right index of the longest palindrome.
+//     let ll = 0, rr = 0;
+//
+//     // Iterate all palindromes with center indices
+//     // [..., i, ...] or [... i, i+1, ...]
+//     for (let i = 0; i < s.length; i++) {
+//         for (let j of [i, i+1]) {
+//             for (l = i, r = j; s[l] && s[l] === s[r]; l--, r++)
+//
+//                 // Found a new palindrome [l, ..., i, j, ..., r]
+//                 // Update the ll, rr if the newly found palindrome is longer than the
+//                 // existing one.
+//                 [ll, rr] = (r-l+1) > (rr-ll+1) ? [l, r] : [ll, rr];
+//         }
+//     }
+//     return s.substring(ll, rr+1);
+// }
+//Solution 1 without comment:
+function longestPalindrome(s) {
+    let ll = 0, rr = 0;
+
+    for (let i = 0; i < s.length; i++)
+        for (let j of [i, i+1])
+            for (l = i, r = j; s[l] && s[l] === s[r]; l--, r++)
+                [ll, rr] = (r-l+1) > (rr-ll+1) ? [l, r] : [ll, rr];
+
+    return s.substring(ll, rr+1);
+}
+console.log(longestPalindrome("babad"));
