950d860d22f49fd65206f607620c37d0e2263807
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-12 09:24:59 -0600

    adds second effort for

--- amazon_sde/medianOfTwoSortedArrays_4.js
+++ amazon_sde/medianOfTwoSortedArrays_4.js
@@ -21,7 +21,8 @@ nums2.length == n
 0 <= n <= 1000
 1 <= m + n <= 2000
 -106 <= nums1[i], nums2[i] <= 106*/
-var findMedianSortedArrays = function(nums1, nums2) {
+//First solution - works in browser, no result here.
+/*var findMedianSortedArrays = function(nums1, nums2) {
     if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1)
     let x = nums1.length
     let y = nums2.length
@@ -46,5 +47,43 @@ var findMedianSortedArrays = function(nums1, nums2) {
         } else
             high = partitionX - 1
     }
+};*/
+//Second solution
+var findMedianSortedArrays = function(nums1, nums2) {
+    //find the smallest length array
+    const len1 = nums1.length;
+    const len2 = nums2.length;
+
+    //swap if len1>len2
+    if(len1 > len2) return findMedianSortedArrays(nums2, nums1);
+
+    //find high and low
+    let lo = 0;
+    let hi = len1;
+
+    while(lo <= hi) {
+
+        //find cut1 and cut2
+        let cut1 = Math.floor((lo+hi)/2);
+        let cut2 = Math.floor((len1+len2)/2) - cut1;
+
+        //find l1 , l2, r1, r2
+        let l1 = (cut1 === 0 ? Number.NEGATIVE_INFINITY : nums1[cut1-1]);
+        let l2 = (cut2 === 0 ? Number.NEGATIVE_INFINITY : nums2[cut2-1]);
+        let r1 = (cut1 === len1 ? Number.MAX_VALUE : nums1[cut1]);
+        let r2 = (cut2 === len2 ? Number.MAX_VALUE : nums2[cut2]);
+
+        //loop again if l1>r2 || l2>r1
+        if(l1>r2) hi = cut1-1;
+        else if(l2>r1) lo = cut1+1;
+        else {
+
+            //return median for even and odd
+            if((len1+len2)%2 === 0) return (Math.max(l1,l2) + Math.min(r1,r2))/2
+            else return (Math.min(r1,r2))
+        }
+    }
+    return -1;
+
 };
-console.log(findMedianSortedArrays());
+console.log(findMedianSortedArrays([1,3],2));
