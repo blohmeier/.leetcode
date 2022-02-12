105b4c2c996fd75091156f165bf026de9fc9ac66
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-12 08:27:35 -0600

    adds second effort for

--- populatingNextRightPointers_116.js
+++ populatingNextRightPointers_116.js
@@ -31,23 +31,38 @@ Follow-up:
 You may only use constant extra space.
 The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
 */
+//Solution 1:
 // Definition for a Node.
- function Node(val, left, right, next) {
-     this.val = val === undefined ? null : val;
-     this.left = left === undefined ? null : left;
-     this.right = right === undefined ? null : right;
-     this.next = next === undefined ? null : next;
- }
-/**
- * @param {Node} root
- * @return {Node}
- */
+//  function Node(val, left, right, next) {
+//      this.val = val === undefined ? null : val;
+//      this.left = left === undefined ? null : left;
+//      this.right = right === undefined ? null : right;
+//      this.next = next === undefined ? null : next;
+//  }
+// /**
+//  * @param {Node} root
+//  * @return {Node}
+//  */
+// var connect = function(root) {
+//     if (root == null || root.left == null) return root;
+//     root.left.next = root.right;
+//     root.right.next = root.next ? root.next.left : null;
+//     connect(root.left);
+//     connect(root.right);
+//     return root;
+// }
+//Solution 2:
 var connect = function(root) {
-    if (root == null || root.left == null) return root;
-    root.left.next = root.right;
-    root.right.next = root.next ? root.next.left : null;
-    connect(root.left);
-    connect(root.right);
-    return root;
+    let ptr = root;
+    while(root && root.left){
+        let temp = root;
+        while(temp) {
+            temp.left.next = temp.right;
+            temp.right.next = temp.next ? temp.next.left : null;
+            temp = temp.next;
+        }
+        root = root.left;
+    }
+    return ptr;
 }
 console.log(connect([1,2,3,4,5,6,7]));
