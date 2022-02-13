4eac6354f1cfdd4142dbdbdb62c54fea43991102
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-13 10:38:02 -0600

    completes

--- amazon_sde/romanToInteger_13.js
+++ amazon_sde/romanToInteger_13.js
@@ -1,13 +1,15 @@
 /*
 see integerToRoman_12.js
  */
-const val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
-const rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+const roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
 
-var romanToInt = function(N) {
-    let ans = ""
-    for (let i = 0; N; i++)
-        while (N >= rom[i]) ans += val[i], N -= rom[i]
+var romanToInt = function(S) {
+    let ans = 0
+    for (let i = S.length-1; ~i; i--) {
+        let num = roman[S.charAt(i)]
+        if (4 * num < ans) ans -= num
+        else ans += num
+    }
     return ans
 };
 console.log(romanToInt("III")); // 3
