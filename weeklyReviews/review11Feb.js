a3025c56f66e095f280e78bd161b56f6211f6d1e
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-13 09:00:36 -0600

    adds fibonacci (first approach) to first weekly review

--- weeklyReviews/review11Feb.js
+++ weeklyReviews/review11Feb.js
@@ -24,9 +24,8 @@
 // primeGenerator ();
 
 //output the first 50 numbers in the fibonacci sequence
-function first50fib() {
-    let num1, num2, nextFib;
-    for (i)
+function fibGenerator() {
+    for (i=1; i>=50; i++)
 }
 // /*704. Binary Search: Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 // You must write an algorithm with O(log n) runtime complexity.*/
