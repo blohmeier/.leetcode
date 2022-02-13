fad42bbc0d8155dadf4bdead040364686f32da2d
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-13 10:08:33 -0600

    updates  with recursive solution

--- reverseLinkedList_206.js
+++ reverseLinkedList_206.js
@@ -22,24 +22,30 @@ The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000
 */
 //Effort 1 - iterative: "Easy [C++/Java/Python/JavaScript] Explained+Animated"
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
+// function reverseList (head) {
+//     let [prev, cur] = [null, head]
+//     while(cur) {
+//         [cur.next, prev, cur] = [prev, cur, cur.next]
+//     }
+//     return prev
+// }
+// console.log(reverseList([1,2,3,4,5]));
+//Effort 2 - recursive: "Javascript Iterative and Recursive Solution"
 function reverseList (head) {
-    let [prev, cur] = [null, head]
-    while(cur) {
-        [cur.next, prev, cur] = [prev, cur, cur.next]
+    if (head == null || head.next == null){
+        return head;
     }
-    return prev
+    let reversedListHead = reverseList(head.next)
+    head.next.next = head;
+    head.next = null;
+    return reversedListHead
 }
 console.log(reverseList([1,2,3,4,5]));
-//Effort 2 - recursive: "Javascript Iterative and Recursive Solution"
-
