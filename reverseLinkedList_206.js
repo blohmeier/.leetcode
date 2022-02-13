f407daabe3837cfd289f64dad20b0da48e4d5c75
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-13 10:05:42 -0600

    updates  with iterative solution

--- reverseLinkedList_206.js
+++ reverseLinkedList_206.js
@@ -21,3 +21,25 @@ Constraints:
 The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000
 */
+//Effort 1 - iterative: "Easy [C++/Java/Python/JavaScript] Explained+Animated"
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function reverseList (head) {
+    let [prev, cur] = [null, head]
+    while(cur) {
+        [cur.next, prev, cur] = [prev, cur, cur.next]
+    }
+    return prev
+}
+console.log(reverseList([1,2,3,4,5]));
+//Effort 2 - recursive: "Javascript Iterative and Recursive Solution"
+
