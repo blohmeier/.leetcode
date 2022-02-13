ad817425a3b5b6d66651f1a8ea6640928c9f24f7
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-13 09:18:05 -0600

    adds fibonacci (second approach) to first weekly review

--- weeklyReviews/review11Feb.js
+++ weeklyReviews/review11Feb.js
@@ -24,16 +24,30 @@
 // primeGenerator ();
 
 //output the first 50 numbers in the fibonacci sequence 3 ways: Class answer, Recursive w/Memoizationo, Binet's formula.
-function fib1() {
-    let n1 = 0, n2 = 1, nextFib;
+// function fib1() {
+//     let n1 = 0, n2 = 1, nextFib;
+//     for (i=1; i<=50; i++) {
+//         console.log("Fibonacci sequence number " + i + " is: " + n1);
+//         nextFib = n1 + n2;
+//         n1 = n2;
+//         n2 = nextFib;
+//     }
+// }
+// fib1();
+function fib2test(el, cache = []) {
+    if (el === 0) return 0;
+    if (el === 1) return 1;
+    if (cache[el]) return cache[el];
+
+    cache[el] = fib2(el - 2, cache) + fib2(el - 1, cache);
+    return cache[el];
+}
+function fib2(){
     for (i=1; i<=50; i++) {
-        console.log("Fibonacci sequence number " + i + " is: " + n1);
-        nextFib = n1 + n2;
-        n1 = n2;
-        n2 = nextFib;
+        console.log("Fibonacci sequence number " + i + " is: " + fib2test(i));
     }
 }
-fib1();
+fib2();
 // /*704. Binary Search: Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 // You must write an algorithm with O(log n) runtime complexity.*/
 // function binarySearch(nums, target) {
