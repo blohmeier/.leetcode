0ef9a88229d7619622c8395649c0c89b692e71ef
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-13 10:36:32 -0600

    updates

--- amazon_sde/romanToInteger_13.js
+++ amazon_sde/romanToInteger_13.js
@@ -1,3 +1,15 @@
 /*
-
+see integerToRoman_12.js
  */
+const val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+const rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+
+var romanToInt = function(N) {
+    let ans = ""
+    for (let i = 0; N; i++)
+        while (N >= rom[i]) ans += val[i], N -= rom[i]
+    return ans
+};
+console.log(romanToInt("III")); // 3
+console.log(romanToInt("LVIII")); // 58
+console.log(romanToInt("MCMXCIV")); // 1994
