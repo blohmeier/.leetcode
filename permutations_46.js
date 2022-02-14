7735e6adf271b8b95a109c982e99bc033a91bc8e
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-14 07:53:19 -0600

    completes

--- permutations_46.js
+++ permutations_46.js
@@ -21,3 +21,25 @@ Constraints:
 -10 <= nums[i] <= 10
 All the integers of nums are unique.
  */
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    let res = [];
+    dfs(nums, new Set(), res);
+    return res;
+}
+function dfs(nums, curr, res) {
+    if (curr.size == nums.length) {
+        res.push(Array.from(curr));
+        return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (curr.has(nums[i])) continue;
+        curr.add(nums[i]);
+        dfs(nums, curr, res);
+        curr.delete(nums[i]);
+    }
+}
+console.log(permute([1,2,3]));
