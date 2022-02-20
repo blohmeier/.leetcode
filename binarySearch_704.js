0dee037914ea70db7f260c24dc39d7ead4541290
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-19 18:02:59 -0600

    completes '704. Binary Search'

--- binarySearch_704.js
+++ binarySearch_704.js
@@ -26,3 +26,24 @@ Constraints:
     -104 < nums[i], target < 104
 All the integers in nums are unique.
     nums is sorted in ascending order.*/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2); // middle of l and r
+        if (nums[m] < target) { // #1
+            l = m + 1;
+        } else if (nums[m] > target) { // #2
+            r = m - 1;
+        } else { // #3
+            return m;
+        }
+    }
+    return -1; // #4
+};
