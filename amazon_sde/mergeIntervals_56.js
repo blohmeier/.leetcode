f7beb9aecd4ad2e0306550fa08047f0dbe3b1787
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-20 08:38:35 -0600

    adds first effort code for '56. Merge Intervals'

--- amazon_sde/mergeIntervals_56.js
+++ amazon_sde/mergeIntervals_56.js
@@ -19,3 +19,19 @@ Constraints:
 intervals[i].length == 2
 0 <= start_sub_i <= end_sub_i <= 104
  */
+//Effort 1
+var merge = function(intervals) {
+    if(!intervals.length) return [];
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    const result = [intervals[0]];
+
+    for(let [start, end] of intervals) {
+        if(start <= result[result.length - 1][1]) {
+            const [startPrev, endPrev] = result.pop();
+            result.push([startPrev, Math.max(end, endPrev)]);
+        } else result.push([start, end]);
+    }
+    return result;
+};
+console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
