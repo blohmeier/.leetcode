4ef56b083ddd60d28f0bafab250704617bf2e403
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-20 08:28:16 -0600

    adds first effort code for '46. Permutations'

--- amazon_sde/permutations_46.js
+++ amazon_sde/permutations_46.js
@@ -18,3 +18,21 @@ Constraints:
 -10 <= nums[i] <= 10
 All the integers of nums are unique.
  */
+//Old effort
+var permute = function(nums) {
+    let res = [];
+    dfs(nums, new Set(), res);
+    return res;
+}
+function dfs(nums, curr, res) {
+    if (curr.size == nums.length) {
+        res.push(Array.from(curr));
+        return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (curr.has(nums[i])) continue;
+        curr.add(nums[i]);
+        dfs(nums, curr, res);
+        curr.delete(nums[i]);
+    }
+}
