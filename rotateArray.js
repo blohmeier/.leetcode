91ee540fcc5c0414cb84f29d4e17fc69b4fa7d12
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-19 18:30:21 -0600

    adds example for '189. Rotate Array'

--- rotateArray.js
+++ rotateArray.js
@@ -89,4 +89,6 @@ var rotate = function(nums, k) {
     }
     return rotate;
 };
-console.log(rotate([1,2,3,4,5,6,7], 3)); //
+console.log(rotate([1,2,3,4,5,6,7], 3)); // [5,6,7,1,2,3,4]
+console.log(rotate([-1,-100,3,99], 2)); // [3,99,-1,-100]
+
