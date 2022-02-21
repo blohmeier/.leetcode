7bc612bfcb92c5174bdb2ad199f3442064bc12f7
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-21 08:34:42 -0600

    updates description for '283. Move Zeroes'

--- moveZeroes_283.js
+++ moveZeroes_283.js
@@ -1,4 +1,5 @@
-// //Solution 1: 124ms
+// // 283. Move Zeroes
+// Effort 1: 124ms
 /*function moveZeroes(nums) {
     var pos = 0;
     for (var i = 0; i < nums.length; i++) {
@@ -12,16 +13,26 @@
     return nums;
 }*/
 // //Second Effort: 83ms.
-// function moveZeroes(nums) {
-//     let pointer = 0
-//     for (let i in nums){
-//         if (nums[i] !== 0){
-//             [nums[pointer], nums[i]] = [nums[i], nums[pointer]]
-//             pointer++
-//         }
-//     }
-//     return nums
-// }
+function moveZeroes(nums) {
+    let pointer = 0
+    for (let i in nums){
+        console.log("before if, pointer = " + pointer);
+        console.log("before if, nums[i] = " + nums[i]);
+        console.log("before if, i = " + i);
+
+        if (nums[i] !== 0){
+            [nums[pointer], nums[i]] = [nums[i], nums[pointer]] // if nums at this index of for loop !==0, swap the number at this index with the number at the next index.
+            pointer++
+            console.log("after if, pointer = " + pointer);
+            console.log("after if, nums[i] = " + nums[i]);
+            console.log("after if, nums[pointer] = " + nums[pointer]);
+            console.log("after if, i = " + i);
+        }
+        console.log(nums);
+    }
+    return nums
+}
+console.log(moveZeroes([0,1,0,3,12]));
 //Solution 3: 94ms.
 // function moveZeroes_283(nums) {
 //     let count=0;
@@ -38,4 +49,4 @@
 //     }
 //     return nums
 // }
-console.log(moveZeroes_283([0,1,0,3,12]));
+// console.log(moveZeroes([0,1,0,3,12]));
