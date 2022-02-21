723c6f3ae539e0061286d140d87b4af83e926825
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-21 08:42:29 -0600

    updates code for '167. Two Sum II - Input Array Is Sorted'

--- twoSum_part2_167.js
+++ twoSum_part2_167.js
@@ -27,36 +27,22 @@ numbers is sorted in non-decreasing order.
 -1000 <= target <= 1000
 The tests are generated such that there is exactly one solution.*/
 
-//Attempt 1 - not working; too slow
-// function twoSum (nums, target) {
-//     let lo = 0,
-//         hi = nums.length - 1; //console.log(nums.length);
-//     while (lo < hi) {
-//         let sum = nums[lo] + nums[hi]; console.log("lo index is " + nums[lo]); console.log("hi index is " +nums[hi]); console.log("sum is " + sum);
-//         if (sum == target) {
-//             console.log(lo); console.log(hi);
-//             //return (lo + 1, hi + 1);
-//         } else if (sum < target) {
-//             ++lo; console.log("sum < target, lo value is now: " + nums[lo] + "\n\n");
-//         } else {
-//             --hi; console.log("sum > target, hi value is now: " + nums[hi] + "\n\n");
-//         }
-//     }
-// };
-
-//Attempt 2 - working
-// let twoSum = (nums, target) => {
-//     let lo = 0,
-//         hi = nums.length - 1
-//     while (nums[lo] + nums[hi] !== target) {
-//         if (nums[lo] + nums[hi] > target) {
-//             hi--
-//         } else {
-//             lo++
-//         }
-//     }
-//     return [lo+1, hi+1]
-// }
+//Effort 1
+let twoSum = (nums, target) => {
+    let lo = 0,
+        hi = nums.length - 1
+    while (nums[lo] + nums[hi] !== target) {
+        if (nums[lo] + nums[hi] > target) {
+            hi--
+        } else {
+            lo++
+        }
+    }
+    return [lo+1, hi+1]
+}
+console.log(twoSum([2,7,11,15],9));
+console.log(twoSum([2,3,4], 6));
+console.log(twoSum([-1,0], -1));
 
 //Attempt 3 - working
 // var twoSum = function(nums, target) {
@@ -72,6 +58,9 @@ The tests are generated such that there is exactly one solution.*/
 //         }
 //     }
 // }
+console.log(twoSum([2,7,11,15],9));
+console.log(twoSum([2,3,4], 6));
+console.log(twoSum([-1,0], -1));
 
 //Attempt 4 - actually works for "1. Two Sum."
 // function twoSum(nums, target) {
