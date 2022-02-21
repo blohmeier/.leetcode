a02ffe95dadab4df4b309b5aa5324571a5328117
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-21 08:54:54 -0600

    renames 'twoSum_I_1' and 'two_sum_II_167

--- amazon_sde/twoSum_I_1.js
+++ amazon_sde/twoSum_I_1.js
@@ -32,4 +32,5 @@ function twoSum(nums, target) {
     }
     return null;
 }
-console.log(twoSum([2,7,11,15],9));
+console.log(twoSum([2,7,11,15],9)); // [0,1]
+console.log(twoSum([3,2,4],6)); // [1,2]
