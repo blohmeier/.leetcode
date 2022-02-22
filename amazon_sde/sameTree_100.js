2bcd7d005a8c4bf9ed45c0290e35a39eee7bd6c0
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-22 08:21:40 -0600

    adds further code for second effort for '100. Same Tree'

--- amazon_sde/sameTree_100.js
+++ amazon_sde/sameTree_100.js
@@ -21,8 +21,17 @@ Constraints:
 The number of nodes in both trees is in the range [0, 100].
 -104 <= Node.val <= 104
  */
-var isSameTree = function(p, q) {
+/*var isSameTree = function(p, q) {
     if (!p && !q) return true;
     if (!p || !q || p.val !== q.val) return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
+console.log(isSameTree([1,2,3],[1,2,3]));
+console.log(isSameTree([1,2],[1,null,2]));
+console.log(isSameTree([1,2,1],[1,1,2]));*/
+var isSameTree = function(p, q) {
+    return JSON.stringify(p)===JSON.stringify(q)
+};
+console.log(isSameTree([1,2,3],[1,2,3]));
+console.log(isSameTree([1,2],[1,null,2]));
+console.log(isSameTree([1,2,1],[1,1,2]));
