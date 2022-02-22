9c93ea4f99daecef3bbf0d869d2a57c62449ba1f
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-22 08:15:04 -0600

    updates description for '100. Same Tree'

--- amazon_sde/sameTree_100.js
+++ amazon_sde/sameTree_100.js
@@ -2,25 +2,17 @@
 100. Same Tree
 Easy
 
-5100
-
-122
-
-Add to List
-
-Share
 Given the roots of two binary trees p and q, write a function to check if they are the same or not.
-
 Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
-
-
 
 Example 1:
 Input: p = [1,2,3], q = [1,2,3]
 Output: true
+
 Example 2:
 Input: p = [1,2], q = [1,null,2]
 Output: false
+
 Example 3:
 Input: p = [1,2,1], q = [1,1,2]
 Output: false
