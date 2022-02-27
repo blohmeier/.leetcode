dd4a5b02ef82eacf5cdbf55b8406e612e471ca99
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-27 09:15:29 -0600

    further updates explanation for 'floodFill_733.js'

--- floodFill_733.js
+++ floodFill_733.js
@@ -1,11 +1,7 @@
 //733. Flood Fill
-/*
-An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
-
+/*An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
 You are also given three integers sr, sc, and newColor. You should perform a flood fill on the image starting from the pixel image[sr][sc].
-
-To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with newColor.
-
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all aforementioned pixels with newColor.
 Return the modified image after performing the flood fill.
 
 Example 1:
@@ -15,10 +11,18 @@ Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e.,
 Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
 
 Example 2:
-
 Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, newColor = 2
 Output: [[2,2,2],[2,2,2]]
- */
+
+Constraints:
+
+m == image.length
+n == image[i].length
+1 <= m, n <= 50
+0 <= image[i][j], newColor < 216
+0 <= sr < m
+0 <= sc < n
+*/
 function floodFill (image, sr, sc, newColor, firstColor = image[sr][sc]) {
     // handle if the coordinate is out of bounds
     // or if it is already the new color
