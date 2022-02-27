cf56c68db110f590f6b3e5b8c0fb656a3d2b776a
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-27 09:05:44 -0600

    further update to explanation for 'permutationInString_567.js'

--- permutationInString_567.js
+++ permutationInString_567.js
@@ -1,26 +1,22 @@
-/*
-567. Permutation in String
-Medium
-
+//567. Permutation in String
+/*Medium
 Given two strings s1 and s2, return true if s2 contains a permutation of s1, else return false.
 IOW, return true if one of s1's permutations is the substring of s2.
 
 Example 1:
-
 Input: s1 = "ab", s2 = "eidbaooo"
 Output: true
 Explanation: s2 contains one permutation of s1 ("ba").
-Example 2:
 
+Example 2:
 Input: s1 = "ab", s2 = "eidboaoo"
 Output: false
 
 Constraints:
-
 1 <= s1.length, s2.length <= 104
 s1 and s2 consist of lowercase English letters.
+*/
 
- */
 function checkInclusion (s1, s2) {
     // in order to consider a string "x" a permutation of string "y" they must meet 2 metrics:
     //    1) they must have the same length.
