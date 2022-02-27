476378ef38d907bef47c960e4f074cd1de367d59
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-27 09:04:11 -0600

    further updates explanation for 'permutationInString_567.js'

--- permutationInString_567.js
+++ permutationInString_567.js
@@ -2,9 +2,8 @@
 567. Permutation in String
 Medium
 
-Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
-
-In other words, return true if one of s1's permutations is the substring of s2.
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, else return false.
+IOW, return true if one of s1's permutations is the substring of s2.
 
 Example 1:
 
