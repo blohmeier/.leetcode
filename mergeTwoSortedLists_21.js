ffda5790c7e91de345beb7f72782947647005e7f
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-28 08:25:16 -0600

    updates explanation for 'mergeTwoSortedLists.js'

--- mergeTwoSortedLists_21.js
+++ mergeTwoSortedLists_21.js
@@ -1,8 +1,5 @@
-/*
-21. Merge Two Sorted Lists
-Easy
-
-You are given the heads of two sorted linked lists list1 and list2.
+//21. Merge Two Sorted Lists
+/*You are given the heads of two sorted linked lists list1 and list2.
 Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 Return the head of the merged linked list.
 
