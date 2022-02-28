51fdc3fa223443bb1cbc1fe40e00576a65a78452
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-28 08:34:08 -0600

    further updates explanation second effort for 'reverseLinkedList_206.js'

--- reverseLinkedList_206.js
+++ reverseLinkedList_206.js
@@ -22,7 +22,7 @@ The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000
 */
 //Effort 1 - iterative: "Easy [C++/Java/Python/JavaScript] Explained+Animated" - leetcode: works. Intellij: doesn't.
-function ListNode(val, next) {
+/*function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
 }
@@ -32,9 +32,9 @@ function reverseList (head) {
         [cur.next, prev, cur] = [prev, cur, cur.next]
     }
     return prev
-}
-//Effort 3 - recursive: "Javascript Iterative and Recursive Solution"
-/*function reverseList (head) {
+}*/
+//Effort 2 - recursive: "Javascript Iterative and Recursive Solution"
+function reverseList (head) {
     if (head == null || head.next == null){
         return head;
     }
@@ -42,5 +42,5 @@ function reverseList (head) {
     head.next.next = head;
     head.next = null;
     return reversedListHead
-}*/
+}
 console.log(reverseList([1,2,3,4,5]));
