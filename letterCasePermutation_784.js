bd93cb2504fee7f4001c243c087bdaa49fa72250
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-01 08:11:54 -0600

    updates description for 'letterCasePermutation_784.js'

--- letterCasePermutation_784.js
+++ letterCasePermutation_784.js
@@ -1,6 +1,4 @@
-/*
-784. Letter Case Permutation
-Medium
+/* Medium.
 
 Given a string s, you can transform every letter individually to be lowercase or uppercase to create another string.
 Return a list of all possible strings we could create. Return the output in any order.
