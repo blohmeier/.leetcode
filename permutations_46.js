1138dbd9fc0c37eb80ce2a3835e85ba2849b7ca1
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-01 08:09:16 -0600

    further updates description for 'permutations_46.js'

--- permutations_46.js
+++ permutations_46.js
@@ -1,8 +1,5 @@
 //46. Permutations
-/*
-Medium
-
-Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+/* Medium. Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
 Example 1:
 Input: nums = [1,2,3]
