c014277a81a5cd238af94fb76fa23f4adf9beddd
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-04 08:29:34 -0600

    adds code for Effort 1 for 'powerOfTwo_231'

--- powerOfTwo_231.js
+++ powerOfTwo_231.js
@@ -27,6 +27,27 @@ RULE 2: 2^n - 1 = {111...1 - n times}
 2^3 - 1 = 8 - 1 = 7 (decimial) = 111 = three ones.
 2^5 - 1 = 32 - 1 = 31 (decimial) = 11111 = five ones.
 
-Based on these two rules, if n is power of two (2^n) if we apply bitwise AND all corresponding bits will be different and we'll receive zero,
+Based on these two rules, if n is power of two (2^n) if we apply bitwise AND all corresponding bits will be different and we'll receive zero.
+EXAMPLES:
+1) n = 2^5
+32 & (32 - 1) = 32 & 31:
+-- 100000
+--- 11111
+--- 00000 == 0
 
+2) n = 2^3
+8 & (8 - 1) = 8 & 7:
+-- 1000
+--- 111
+--- 000 == 0
+
+3) 25?
+25 & (25 - 1) = 25 & 24:
+-- 11001
+-- 11000
+-- 11000 != 0
  */
+let isPowerOfTwo = n => n > 0 ? !(n & n-1) : false;
+console.log(isPowerOfTwo(1));
+console.log(isPowerOfTwo(16));
+console.log(isPowerOfTwo(3));
