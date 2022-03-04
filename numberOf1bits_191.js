35a259bb93558a558c26c144c2b41462b50dbfaa
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-04 08:42:32 -0600

    adds code for Effort 2 for 'numberOf1bits_191'

--- numberOf1bits_191.js
+++ numberOf1bits_191.js
@@ -19,15 +19,33 @@ Explanation: The input binary string 11111111111111111111111111111101 has thirty
 Constraints:
 The input must be a binary string of length 32.
 Follow up: If this function is called many times, how would you optimize it?*/
+
 //Effort 1: 83ms. Works, but third example displays '0' in IntelliJ?
-var hammingWeight = function(n) {
+/*var hammingWeight = function(n) {
     let num_of_1s = 0;
     for( let i=0 ; i < 32; i++ ){
         num_of_1s += n & 1;
         n >>= 1;
     }
     return num_of_1s
-}
+}*/
+
+//Effort 2: same as Effort 1.
+var hammingWeight = function(n) {
+
+    let count = 0;
+
+    while( n ){
+
+        // this will take out the right-most 1 of n
+        n = n & (n-1);
+
+        // update counter
+        count += 1
+    }
+
+    return count
+};
 console.log(hammingWeight(0o00000000000000000000000000001011)); //3
 console.log(hammingWeight(0o00000000000000000000000010000000)); //1
 console.log(hammingWeight(11111111111111111111111111111101)); //31
