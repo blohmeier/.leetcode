55c975d8ba4e1941adda20afb101326f76241e1d
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-04 08:23:17 -0600

    updates description for Effort 1 for 'powerOfTwo_231'

--- powerOfTwo_231.js
+++ powerOfTwo_231.js
@@ -19,16 +19,14 @@ Constraints:
 -231 <= n <= 231 - 1
  */
 //Effort 1
-/*Description: Just a binary trick:
-
-2^n = 1{000...0 - n times}, binary.
+/*Description: Just a binary trick. Use two rules:
+RULE 1: If 2^n = 1{000...0 - n times}, is binary. EXAMPLES:
 2^2 = 4 (decimial) = 100 binary - two zeros.
 2^5 = 32 (decimial) = 100000 binary - five zeros.
-
-2^n - 1 = {111...1 - n times}
+RULE 2: 2^n - 1 = {111...1 - n times}
 2^3 - 1 = 8 - 1 = 7 (decimial) = 111 = three ones.
 2^5 - 1 = 32 - 1 = 31 (decimial) = 11111 = five ones.
 
-Based on this two rules, if n is power of two (2^n) if we apply bitwise AND all corresponding bits will be different and we'll receive zero, 
+Based on these two rules, if n is power of two (2^n) if we apply bitwise AND all corresponding bits will be different and we'll receive zero,
 
  */
