2275def5b418f35a9df7b431413bc75521aba76a
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-04 07:27:09 -0600

    further updates description for '70_climbingStairs' - Effort 2 complete

--- climbingStairs_70.js
+++ climbingStairs_70.js
@@ -41,18 +41,13 @@ Constraints:
     // Outside of our loop we return curr which stored our cumulative total while we iterated.
     return curr;
 }*/
-//Effort 2:
-/*
-DP
 
-dp[i] represents the total number of different ways to take i steps
-So, we want to get dp[n].
+
+//Effort 2 - bst approach?:
+/*DP - dp[i] represents the total number of different ways to take i steps. So, we want to get dp[n].
 dp[n] = dp[n-1] + dp[n-2] because we can either take 1 or 2 steps.
-
-We have two base cases: dp[1] = 1 and dp[2] = 2 because
-there is one way to take 1 step and there are two ways to take 2 steps (1 step + 1 step OR 2 step)
-*/
-/*var climbStairs = function(n) {
+We have two base cases: dp[1] = 1 and dp[2] = 2 because there is one way to take 1 step and there are two ways to take 2 steps (1 step + 1 step OR 2 step)*/
+var climbStairs = function(n) {
     let dp = new Array(n + 1);
     dp[1] = 1, dp[2] = 2;
     for (let i = 3; i <= n; i++) {
@@ -61,7 +56,9 @@ there is one way to take 1 step and there are two ways to take 2 steps (1 step +
     return dp[n];
     // T.C: O(N)
     // S.C: O(N)
-};*/
+};
+
+
 //Effort 3:
 /*
 Recursion
