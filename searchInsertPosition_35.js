0c284fc893a534394a712525129dfa7f1504972e
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-05 08:25:29 -0600

    updates Effort 1 description for '35. Search Insert Position'

--- searchInsertPosition_35.js
+++ searchInsertPosition_35.js
@@ -20,6 +20,19 @@ Constraints:
 3. nums contains distinct values sorted in ascending order.*/
 
 //Effort 1:
+/*function searchInsert(nums, target) {
+    return binarySearch(nums, target, 0, nums.length - 1);
+};
+function binarySearch(array, target, start, end) {
+    if (start > end) return start;
+    const midPoint = Math.floor((start + end)/2);
+    // found target
+    if (array[midPoint] === target) return midPoint;
+    // search the left side
+    if (array[midPoint] > target) return binarySearch(array, target, start, midPoint - 1);
+    // search the right side
+    if (array[midPoint] < target) return binarySearch(array, target, midPoint + 1, end);
+}*/
 //Effort 2: Effort 1 simplified similar to 704. Binary Search
 function searchInsert(nums, target) {
     return binarySearch(nums, target, 0, nums.length - 1);
