af2a7b921235a06017a6bf126acfa3d62319c1ae
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-05 07:58:57 -0600

    adds code for Effort 4 description for 'binarySearch_70'

--- binarySearch_704.js
+++ binarySearch_704.js
@@ -58,7 +58,35 @@ Constraints:
     } return -1
 }*/
 //Effort 4: Effort 1 with console output:
+var search = function(nums, target) {
 
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2); // middle of l and r
+        if (nums[m] < target) { // #1
+            l = m + 1;
+            console.log("start of #1 loop");
+            console.log("middle is: " + m);
+            console.log("index of middle is less than target, and is: " + nums[m]);
+            console.log("left is: " + l);
+            console.log("right is: " + r);
+            console.log("length is: " + nums.length);
+        } else if (nums[m] > target) { // #2
+            r = m - 1;
+            console.log("start of #2 loop");
+            console.log("middle is: " + m);
+            console.log("index of middle is greater than target, and is: " + nums[m]);
+            console.log("left is: " + l);
+            console.log("right is: " + r);
+            console.log("length is: " + nums.length);
+        } else { // #3
+            return m;
+        }
+    }
+    return -1; // #4
+};
 console.log(search([-1,0,3,5,9,12],9)); //4
 console.log(search([-1,0,3,5,9,12],2)); //-1
 console.log(search([-1,0,3,5,9,12],3)); //2
