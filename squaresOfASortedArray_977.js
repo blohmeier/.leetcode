70eddb1772e9c54654b2993bb9fca1f275d49d98
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-07 07:42:05 -0600

    updates code for first example for 'squaresOfASortedArray_977'

--- squaresOfASortedArray_977.js
+++ squaresOfASortedArray_977.js
@@ -20,22 +20,23 @@ Follow up: Squaring each element and sorting the new array is very trivial, coul
  */
 
 //Solution 1
-// function sortedSquares(nums) {
-//     let result = [],
-//         l = 0,
-//         r = nums.length - 1,
-//         p = r;
-//     while (l <= r) {
-//         if (nums[l] ** 2 > nums[r] ** 2) {
-//             result[p--] = nums[l++] ** 2;
-//         } else {
-//             result[p--] = nums[r--] ** 2;
-//         }
-//     }
-//     return result;
-// };
-//Solution 2
 function sortedSquares(nums) {
+    let result = [],
+        l = 0,
+        r = nums.length - 1,
+        p = r;
+    while (l <= r) {
+        if (nums[l] ** 2 > nums[r] ** 2) {
+            result[p--] = nums[l++] ** 2;
+        } else {
+            result[p--] = nums[r--] ** 2;
+        }
+    }
+    return result;
+}
+
+//Solution 2
+/*function sortedSquares(nums) {
     let length = nums.length,
         result = [length],
         leftPointer = 0,
@@ -54,6 +55,6 @@ function sortedSquares(nums) {
     return result;
     // Time: O(n)
     // Space: O(n)
-}
+}*/
 console.log(sortedSquares([-4,-1,0,3,10])); //[0,1,9,16,100]
 console.log(sortedSquares([-7,-3,2,3,11])); //[4,9,9,49,121]
