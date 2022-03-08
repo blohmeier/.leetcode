dcd184752400b86e4f923f84635e1d2d8e57f966
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-08 08:54:20 -0600

    updates Effort 3 code for 'moveZeroes_283'

--- moveZeroes_283.js
+++ moveZeroes_283.js
@@ -51,19 +51,19 @@ Follow up: Could you minimize the total number of operations done?*/
 }*/
 
 //Effort 3: 94ms.
-// function moveZeroes_283(nums) {
-//     let count=0;
-//     for(let i=0;i<nums.length;i++){
-//         if(count===nums.length){
-//             break
-//         }
-//         count++
-//         if(nums[i]===0){
-//             nums.splice(nums.length,0,nums.splice(i,1)[0])
-//             i--
-//         }
-//
-//     }
-//     return nums
-// }
+/*function moveZeroes_283(nums) {
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+        if(count===nums.length){
+            break
+        }
+        count++
+        if(nums[i]===0){
+            nums.splice(nums.length,0,nums.splice(i,1)[0])
+            i--
+        }
+    }
+    return nums
+}*/
+
 // console.log(moveZeroes([0,1,0,3,12]));
