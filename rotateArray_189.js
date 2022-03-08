b45aa0e64af4b4087525e85e7df645abafb3b925
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-08 08:45:07 -0600

    completes Attempt 4 description for 'rotateArray_189'

--- rotateArray_189.js
+++ rotateArray_189.js
@@ -67,7 +67,7 @@ Could you do it in-place with O(1) extra space?
 // console.log(rotate([1,2,3,4,5,6,7], 3)); //
 //console.log(rotate());
 //Above not working. Attempt 4 - works in leetcode but no output in IntelliJ:
-var rotate = function(nums, k) {
+/*var rotate = function(nums, k) {
 
     k = k%nums.length
 
@@ -84,7 +84,7 @@ var rotate = function(nums, k) {
             r--
         }
     }
-}
+}*/
 console.log(rotate([1,2,3,4,5,6,7], 3)); // [5,6,7,1,2,3,4]
 console.log(rotate([-1,-100,3,99], 2)); // [3,99,-1,-100]
 
