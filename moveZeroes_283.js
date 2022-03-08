dd210a661a5dc02a5027cc364bc4b891ad0e0b6e
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-08 08:51:21 -0600

    completes description for 'moveZeroes_283'

--- moveZeroes_283.js
+++ moveZeroes_283.js
@@ -1,30 +1,19 @@
 // 283. Move Zeroes
-/*
-Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-
-Note that you must do this in-place without making a copy of the array.
-
-
+/*Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements. Note that you must do this in-place without making a copy of the array.
 
 Example 1:
-
 Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
-Example 2:
 
+Example 2:
 Input: nums = [0]
 Output: [0]
 
-
 Constraints:
+1. 1 <= nums.length <= 10^4
+2. -2^31 <= nums[i] <= 2^31 - 1
 
-1 <= nums.length <= 104
--231 <= nums[i] <= 231 - 1
-
-
-Follow up: Could you minimize the total number of operations done?
- */
-
+Follow up: Could you minimize the total number of operations done?*/
 
 // Effort 1: 124ms
 /*function moveZeroes(nums) {
