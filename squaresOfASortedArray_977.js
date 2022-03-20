c221b0456d2a839dee34aeeef78192a53cfbd66d
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-20 10:23:23 -0500

    updates 977. Squares of a Sorted Array

--- squaresOfASortedArray_977.js
+++ squaresOfASortedArray_977.js
@@ -69,7 +69,7 @@ function sortedSquares(nums) {
     return result;
 }
 
-//Solution 2
+//Solution 3
 /*function sortedSquares(nums) {
     let length = nums.length,
         result = [length],
@@ -91,5 +91,4 @@ function sortedSquares(nums) {
     // Space: O(n)
 }*/
 console.log(sortedSquares([-4,-1,0,3,10])); //[0,1,9,16,100]
-/*
-console.log(sortedSquares([-7,-3,2,3,11])); //[4,9,9,49,121]*/
+/*console.log(sortedSquares([-7,-3,2,3,11])); //[4,9,9,49,121]*/
